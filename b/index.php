e0e8af5c50ae940a37500b55f111d87bc6990a6e
thiagoalbrecht <thisalviano@hotmail.com>
2022-01-10 22:35:52 +0100

    Board creator test

--- b/index.php
+++ b/index.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 preg_match('/([^\/]+)\/?/', $_GET['id'], $output_array); // Find correct 'id' even if query contains slash(es)
-$boardlink = "boardpin.xyz/b/".$output_array[1];
+$boardlink = "boardpin.xyz/b/" . $output_array[1];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,19 +24,19 @@ $boardlink = "boardpin.xyz/b/".$output_array[1];
             <div class="container"><a class="navbar-brand" href="/" style="color: rgb(255,255,255);"><i class="fas fa-map-pin me-2"></i>Boardpin</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navcol-1">
                     <ul class="navbar-nav me-auto"></ul>
-                    <div class="input-group" style="max-width: 370px;"><span class="input-group-text">Board link</span><input class="form-control" type="text" value="<?php echo $boardlink;?>" disabled><button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#qrcode"><i class="fas fa-qrcode"></i></button><button class="btn btn-primary" type="button" v-on:click="copytoClipboard('<?php echo $boardlink;?>')"><i class="far fa-copy"></i></button></div>
+                    <div class="input-group" style="max-width: 370px;"><span class="input-group-text">Board link</span><input class="form-control" type="text" value="<?php echo $boardlink; ?>" disabled><button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#qrcode"><i class="fas fa-qrcode"></i></button><button class="btn btn-primary" type="button" v-on:click="copytoClipboard('<?php echo $boardlink; ?>')"><i class="far fa-copy"></i></button></div>
                 </div>
             </div>
         </nav>
         <div class="container mt-5" style="max-width: 600px;">
             <div class="input-group input-group-lg" id="commands">
-            <input class="form-control" type="text" placeholder="Type something (or /help)" style="background: var(--bs-dark);color: var(--bs-white);" v-model="newNoteText" v-on:keyup.enter="addNote()"><button class="btn btn-primary" type="button" v-on:click="addNote()" onclick="return false;"><i class="fas fa-plus"></i></button>
-        </div>
+                <input class="form-control" type="text" placeholder="Type something (or /help)" style="background: var(--bs-dark);color: var(--bs-white);" v-model="newNoteText" v-on:keyup.enter="addNote()"><button class="btn btn-primary" type="button" v-on:click="addNote()" onclick="return false;"><i class="fas fa-plus"></i></button>
+            </div>
             <div id="main-content" class="mt-4">
                 <transition-group tag="ul" name="list-animation" class="list-group">
-                        <li v-for="note in noteItems" v-bind:key="note.Id" class="list-group-item d-flex justify-content-between align-items-center list-item-animation">
-                            <div><input class="form-check-input me-2" type="checkbox" v-model="note.IsDone"><label v-bind:class="{ completed: note.IsDone }" class="form-check-label">{{ note.Text }}</label></div><button class="btn btn-link btn-sm" v-on:click="removeNote(note)" type="button" style="color: var(--bs-gray);"><i class="fas fa-times"></i></button>
-                        </li>
+                    <li v-for="note in noteItems" v-bind:key="note.Id" class="list-group-item d-flex justify-content-between align-items-center list-item-animation">
+                        <div><input class="form-check-input me-2" type="checkbox" v-model="note.IsDone"><label v-bind:class="{ completed: note.IsDone }" class="form-check-label">{{ note.Text }}</label></div><button class="btn btn-link btn-sm" v-on:click="removeNote(note)" type="button" style="color: var(--bs-gray);"><i class="fas fa-times"></i></button>
+                    </li>
                 </transition-group>
             </div>
         </div>
@@ -46,8 +46,8 @@ $boardlink = "boardpin.xyz/b/".$output_array[1];
                     <div class="modal-header">
                         <h4 class="modal-title">Board link</h4><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <div class="modal-body text-center"><img src="https://api.qrserver.com/v1/create-qr-code/?size=250x250&amp;data=https://<?php echo $boardlink;?>" style="width: 100%;max-width: 250px;" />
-                        <h4 class="mt-2"><?php echo $boardlink;?></h4>
+                    <div class="modal-body text-center"><img src="https://api.qrserver.com/v1/create-qr-code/?size=250x250&amp;data=https://<?php echo $boardlink; ?>" style="width: 100%;max-width: 250px;" />
+                        <h4 class="mt-2"><?php echo $boardlink; ?></h4>
                     </div>
                 </div>
             </div>
---
+++ new/index.php
@@ -0,0 +1,39 @@
+<?php
+require '../secret/credentials.php';
+
+$board_id = generateBoardID();
+createNewBoard($board_id);
+
+function generateBoardID($length = 6)
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+
+function createNewBoard($board_id)
+{
+    $conn = new mysqli(servername, username, password, dbname);
+    mysqli_set_charset($conn, "utf8mb4");
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $select_query = "SELECT trimmed_json FROM boards WHERE board='$board_id'";
+
+    $result = $conn->query($select_query);
+
+    if ($result->num_rows > 0) {
+        $board_id = generateBoardID();
+        createNewBoard($board_id);
+    } else {
+        $insert_query = "INSERT INTO `boards`(`board`, `trimmed_json`) VALUES ('$board_id','')'";
+        $result = $conn->query($insert_query);
+    }
+}
