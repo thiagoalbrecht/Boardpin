6aefb6903a6b38074317287f8f6e44d75477f7a9
Thiago Albrecht <thisalviano@hotmail.com>
2022-01-27 18:07:20 +0100

    Fixed <script> placement

--- b/index.php
+++ b/index.php
@@ -54,10 +54,10 @@ $boardlink = "boardpin.xyz/b/" . $output_array[1];
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        let boardlink = "<?php echo $output_array[1]; ?>";
+    </script>
+    <script src="/app.js"></script>
 </body>
-<script>
-    let boardlink = "<?php echo $output_array[1]; ?>";
-</script>
-<script src="/app.js"></script>
 
 </html>
